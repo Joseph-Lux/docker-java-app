32b8a9505778352f17d88c7ff1130e6be8987c1b
Joseph Lux <josephandrewlux@gmail.com>
2024-06-20 10:29:31 -0500

    Sending test change through CI pipe

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello, Docker!");
+		System.out.println("I have made a change that made it through the CI pipe.");
 	}
 }
